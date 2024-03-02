ac06989ccb65a40d30a1d476597d2fd81e24a33b
faresh9 <far0213025@ju.edu.jo>
2024-03-02 22:10:09 +0300

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemaTypes'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'IHSAN',
+
+  projectId: 'fzdbee7y',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
