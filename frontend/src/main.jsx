579e9d6297cb97d40c344d571887c2eb8bb3bdb2
faresh9 <far0213025@ju.edu.jo>
2024-04-05 23:18:48 +0300

    Add @vercel/speed-insights package

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 //import 'rsuite/dist/rsuite.min.css'; 
 import {studioTheme, ThemeProvider} from '@sanity/ui'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 <ThemeProvider theme={studioTheme}>
 <App />
+<SpeedInsights/>
 </ThemeProvider>
       
    
