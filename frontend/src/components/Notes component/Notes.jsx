822df90a9f22b25b8e86e84eebb9ded58bd04a6d
faresh9 <far0213025@ju.edu.jo>
2024-05-22 18:07:33 +0300

    refactor: Remove commented out code for initial notes in Notes component

--- frontend/src/components/Notes component/Notes.jsx
+++ frontend/src/components/Notes component/Notes.jsx
@@ -7,26 +7,26 @@ import NotesHeader from './NotesHeader';
 import '../../../styles/note.css'
 function Notes() {
 	const [notes, setNotes] = useState([
-		{
-			id: nanoid(),
-			text: 'This is my first note!',
-			date: '15/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my second note!',
-			date: '21/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my third note!',
-			date: '28/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my new note!',
-			date: '30/04/2021',
-		},
+		// {
+		// 	id: nanoid(),
+		// 	text: 'This is my first note!',
+		// 	date: '15/04/2021',
+		// },
+		// {
+		// 	id: nanoid(),
+		// 	text: 'This is my second note!',
+		// 	date: '21/04/2021',
+		// },
+		// {
+		// 	id: nanoid(),
+		// 	text: 'This is my third note!',
+		// 	date: '28/04/2021',
+		// },
+		// {
+		// 	id: nanoid(),
+		// 	text: 'This is my new note!',
+		// 	date: '30/04/2021',
+		// },
 	]);
 
 	const [searchText, setSearchText] = useState('');
