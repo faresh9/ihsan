9536123f0e5d7fe2ff2d70d6bef89c7e08adb12f
faresh9 <far0213025@ju.edu.jo>
2024-06-29 00:06:50 +0300

    chore: Update database connection configuration

--- backend/ihsanModel.js
+++ backend/ihsanModel.js
@@ -1,11 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "fares1234",
-  host: "localhost",
-  port: 5432,
-  database: "ihsan_db"
-});
+  connectionString: process.env.POSTGRES_URL,
+})
 
 module.exports = pool;
