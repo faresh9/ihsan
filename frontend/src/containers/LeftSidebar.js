23f26900bfe8308367b2581b447d1d91b1cb0575
faresh9 <far0213025@ju.edu.jo>
2024-07-11 20:25:33 +0300

    .

--- frontend/src/containers/LeftSidebar.js
+++ frontend/src/containers/LeftSidebar.js
@@ -24,7 +24,7 @@ function LeftSidebar(){
 
                 <li className="mb-2 font-semibold text-xl">
                     
-                    <Link to={'/app/welcome'}><img className="mask mask-squircle w-10" src="https://www.reshot.com/preview-assets/icons/FWRL3UY7XC/koran-FWRL3UY7XC.svg" alt=" ihsan Logo"/>Ihsan</Link> </li>
+                    <Link to={'/app/welcome'}><img className="mask mask-squircle w-10" src="https://www.reshot.com/preview-assets/icons/FWRL3UY7XC/koran-FWRL3UY7XC.svg" alt=" ihsan Logo"/>IHSAN</Link> </li>
                 {
                     routes.map((route, k) => {
                         return(
